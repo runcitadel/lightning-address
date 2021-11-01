35c1706588a1a718a98280f3aa46e366efc563d6
Aaron Dewes <aaron.dewes@web.de>
2021-11-01 10:25:10 +0000

    Revert "Try using curl, Node somehow can't do it"
    This reverts commit 61bdefeaf3df3a2fc33e7892155eb5ee00b759dd.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
 import Koa from "koa";
 import Router from "@koa/router";
-import * as child_process from "child_process";
+import fetch from "node-fetch";
+import SocksProxyAgentPkg from "socks-proxy-agent";
+const SocksProxyAgent = SocksProxyAgentPkg.SocksProxyAgent;
 
+// Connect to the local tor daemon
+
+const proxy = process.env.socks_proxy || 'socks5h://localhost:9050';
 const app = new Koa();
 const router = new Router();
+const agent = new SocksProxyAgent(proxy);
 
-// child_process.exec
 const usernames: Record<string, string> = {
     "test123": "st5owtpsa2e62yf64luxogbecj7lk3t5vmesshsnrzu2untyf2i4t4ad.onion"
 }
-router.get("/.well-known/lnurlp/:username", async (ctx, next) => {
+router.get("/.well-known/lnurlp/:username", async (ctx) => {
     const username: string = ctx.params.username;
     if (usernames[username.toLowerCase()]) {
-        // Use curl -s --socks5-hostname 127.0.0.1:9050 http://${usernames[username.toLowerCase()]}/.well-known/lnurlp/${username}
-        const url = `http://${usernames[username.toLowerCase()]}/.well-known/lnurlp/${username}`;
-        // Run curl in a subprocess
-        const child = child_process.execSync(`curl -s --socks5-hostname 127.0.0.1:9050 ${url}`);
-        ctx.body = JSON.parse(child.toString());
+        // send a request to the users onion
+        const apiResponse = await fetch(`http://${usernames[username.toLowerCase()]}/.well-known/lnurlp/${username}`, {
+            agent
+        });
+        const json = await apiResponse.json();
+        ctx.body = json;
     } else {
         ctx.status = 404;
     }
-    await next();
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
