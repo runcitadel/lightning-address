2eeb340c08e9928f922b8b319fbfdd163b9bd60b
Aaron Dewes <aaron.dewes@web.de>
2021-11-01 09:42:15 +0000

    Some fixes

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,19 @@ const SocksProxyAgent = SocksProxyAgentPkg.SocksProxyAgent;
 
 // Connect to the local tor daemon
 
-const proxy = process.env.socks_proxy || 'socks5h://127.0.0.1:9050';
+const proxy = process.env.socks_proxy || 'socks5h://localhost:9050';
 const app = new Koa();
 const router = new Router();
 const agent = new SocksProxyAgent(proxy);
 
 const usernames: Record<string, string> = {
-    "test123": "t5owtpsa2e62yf64luxogbecj7lk3t5vmesshsnrzu2untyf2i4t4ad.onion"
+    "test123": "st5owtpsa2e62yf64luxogbecj7lk3t5vmesshsnrzu2untyf2i4t4ad.onion"
 }
 router.get("/.well-known/lnurlp/:username", async (ctx) => {
     const username: string = ctx.params.username;
     if (usernames[username.toLowerCase()]) {
         // send a request to the users onion
-        const apiResponse = await fetch(`${usernames[username.toLowerCase()]}/.well-known/lnurlp/${username}`, {
+        const apiResponse = await fetch(`http://${usernames[username.toLowerCase()]}/.well-known/lnurlp/${username}`, {
             agent
         });
         const json = await apiResponse.json();
